16ff70ff3202a59c5624cb53ccdbd11b686ce41a
Rafael Troiani <rafael.troiani@nubank.com.br>
2022-03-05 16:59:38 -0300

    Add files via upload

--- setup.py
+++ setup.py
@@ -4,17 +4,19 @@ DESCRIPTION = 'Creates a static .html file with charts using Google Charts API'
 
 setuptools.setup(
     name="plot2html",
-    version="0.0.3",
+    version="0.0.4",
     author="Rafael Rodrigues Troiani",
     author_email="rafael.r.troiani@gmail.com",
     description=DESCRIPTION,
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src"),
-    install_requires=[],
+    url='https://github.com/rafatro/plot2html',
+    install_requires=['pandas','datetime'],
     keywords=['python', 'Google Charts', 'html'],
     classifiers=[
-        "Development Status :: 1 - Planning",
+        "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3"
     ]
 )
