65383e2546a91fb6a84a45e74135cbe4d38b5d41
Rafael Troiani <rafael.troiani@nubank.com.br>
2022-03-05 23:33:12 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,17 @@ import setuptools
 
 DESCRIPTION = 'Creates a static .html file with charts using Google Charts API'
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setuptools.setup(
     name="plot2html",
-    version="0.0.4",
+    version="0.0.9",
     author="Rafael Rodrigues Troiani",
     author_email="rafael.r.troiani@gmail.com",
     description=DESCRIPTION,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src"),
     url='https://github.com/rafatro/plot2html',
